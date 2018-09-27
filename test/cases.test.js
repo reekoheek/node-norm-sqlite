9e450691477b155e0731cbd2e4a7214619d41c3c
Januar Siregar <januar.siregar@xinix.co.id>
2018-09-27 08:46:26 +0700

    fixing

--- test/cases.test.js
+++ test/cases.test.js
@@ -10,7 +10,9 @@ describe('Cases', () => {
   beforeEach(async () => {
     db = await sqlite.open(':memory:');
     await db.run('CREATE TABLE foo (id INTEGER PRIMARY KEY AUTOINCREMENT, foo TEXT);');
+    await db.run('CREATE TABLE test1 (id INTEGER PRIMARY KEY AUTOINCREMENT, `order` TEXT,`key` TEXT,`group` TEXT);');
     await db.run('INSERT INTO foo (foo) VALUES (?), (?)', ['1', '2']);
+    await db.run('INSERT INTO test1 (`order`,`key`,`group`) VALUES (?,?,?), (?,?,?)', ['1', '2', '3', '4', '5', '6']);
 
     manager = new Manager({
       connections: [
@@ -22,12 +24,42 @@ describe('Cases', () => {
     });
   });
 
+  it('create new record with esacpe character', async () => {
+    await manager.runSession(async session => {
+      let { affected, rows } = await session.factory('test1').insert({ order: 'bar', key: '1' }).insert({ order: 'bar1', key: '2' }).save();
+      assert.strictEqual(affected, 2);
+      assert.strictEqual(rows.length, 2);
+      let foos = await db.all('SELECT * FROM test1');
+      assert.strictEqual(foos.length, 4);
+    });
+  });
+
+  it('update record with escape character', async () => {
+    await manager.runSession(async session => {
+      let { affected } = await session.factory('test1', 1).set({ key: 'bar2' }).save();
+      assert.strictEqual(affected, 1);
+      let foo = await db.get('SELECT * FROM test1 WHERE id = 1');
+      assert.strictEqual(foo.key, 'bar2');
+    });
+  });
+
+  it('delete record with escape character', async () => {
+    await manager.runSession(async session => {
+      await session.factory('test1', {group: '3'}).delete();
+
+      let foos = await db.all('SELECT * FROM test1');
+      console.info(foos);
+      assert.strictEqual(foos.length, 1);
+    });
+  });
+
   it('create new record', async () => {
     await manager.runSession(async session => {
       let { affected, rows } = await session.factory('foo').insert({ foo: 'bar' }).insert({ foo: 'bar1' }).save();
       assert.strictEqual(affected, 2);
       assert.strictEqual(rows.length, 2);
       let foos = await db.all('SELECT * FROM foo');
+
       assert.strictEqual(foos.length, 4);
     });
   });
@@ -44,6 +76,7 @@ describe('Cases', () => {
       let { affected } = await session.factory('foo', 2).set({ foo: 'bar' }).save();
       assert.strictEqual(affected, 1);
       let foo = await db.get('SELECT * FROM foo WHERE id = 2');
+
       assert.strictEqual(foo.foo, 'bar');
     });
   });
